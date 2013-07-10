367db2f99e2815f87d548d383d67daae57a87b51
kstock <kstock@kstock-U47A.(none)>
2013-07-09 21:29:11 -0700

    minor changes

--- games/predict.py
+++ games/predict.py
@@ -1,3 +1,4 @@
+from __future__ import division
 from collections import defaultdict
 from random import choice
 import re
@@ -5,33 +6,39 @@ import re
 COMPUTER = 0
 PLAYER   = 1
 
+def notAllZero(iterable):
+    for i in iterable:
+        if i != 0:
+            return True
+    return False
+
+
 class Predict:
 
-    def __init__(self,domain = None, goal = None,lookbehind = None):
+    def __init__(self,alphabet = None, winningScore = None,lookbehind = None):
         self.history = ""
 
+        if not alphabet: #else given as a sting already
+            print "what do you want the alphabet to be? type the characters"
+            alphabet = raw_input()
 
-        if not domain: #else given as a sting already
-            print "what do you want the domain to be? type the characters"
-            domain = raw_input()
-
-        self.search=re.compile(r'[^'+domain+']').search
-        self.domain = list( domain )
+        self.search=re.compile(r'[^'+alphabet+']').search
+        self.alphabet = list( alphabet )
 
         #used to make sure incorrect char not given by user
 
         self.index = {}
-        for i,elem in enumerate(self.domain):
+        for i,elem in enumerate(self.alphabet):
             self.index[elem] = i
 
-        self.freqs = defaultdict(lambda :[0]*len(domain))
+        self.freqs = defaultdict(lambda :[0]*len(alphabet))
 
 
-        if not goal:
+        if not winningScore:
             print "What score do you want to play to?"
-            self.goal = int( raw_input() ) #TODO stupid input validation
+            self.winningScore = int( raw_input() ) #TODO stupid input validation
         else:
-            self.goal = goal
+            self.winningScore = winningScore
 
         if not lookbehind:
             print "What int amount of memory should I have"
@@ -43,25 +50,23 @@ class Predict:
     def decideAction(self,playerChoice):
 
         if len(self.history) <= self.lookbehind:
-            compChoice = choice(self.domain)
+            compChoice = choice(self.alphabet)
         else:
             lastFew       = self.history[-self.lookbehind:]
             pastExperience = self.freqs[lastFew]
-            print pastExperience
 
             indexOfMax = max((v,i) for i,v in enumerate(pastExperience))[1]
-            print "max index === " + str(indexOfMax)
-            if  self.notAllZero(pastExperience): #make sure does random if never seen
-                compChoice = self.domain[ indexOfMax ]
+
+            if  notAllZero(pastExperience): #make sure does random if never seen
+                compChoice = self.alphabet[ indexOfMax ]
             else:
                 print "random"
-                compChoice = choice(self.domain)
+                compChoice = choice(self.alphabet)
 
             #increase the tally for the amount of time playerChoice
             #appeared after the last 4 characters in the history
             mostLikely =  self.index[playerChoice]
             self.freqs[lastFew][mostLikely] += 1
-            print self.freqs
 
         self.history += playerChoice
         return compChoice
@@ -70,24 +75,12 @@ class Predict:
     def special_match(self,strg,search):
         return not bool(search(strg))
 
-    #TODO check iteration stuff
-    def notAllZero(self,iterable):
-        for i in iterable:
-            if i != 0:
-                return True
-
-        return False
-
-    def playAtOnce(self):
-
-        print "Type a random string. I'll look at it character by character and make guesses"
-        myRandStr = raw_input()
-
         #check if it has illegal characters
 #        if  self.special_match(myRandStr,self.search):
             #print "you gave illegal characters. I win by default"
             #return
 
+    def play(self,myRandStr):
         scoreComp = 0
         scorePlayer = 0
 
@@ -103,45 +96,101 @@ class Predict:
 
         print "My score is: " + str(scoreComp) + ",   Your score is: " + str(scorePlayer)
         self.gameOver(scorePlayer,scoreComp)
+        self.findProbs(self.history)
+        self.reset()
 
-#@staticmethod
     def whoWinsRound(self,compChoice,playerChoice):
         if compChoice == playerChoice:
             return COMPUTER
         else:
             return PLAYER
 
+    def playAtOnce(self):
 
+        print "Type a random string. I'll look at it character by character and make guesses"
+        myRandStr = raw_input()
+        self.play(myRandStr)
 
-    def playInteractive(self):
+    def againstPython(self,rounds = 10):
+        myRandStr = ''.join( choice(self.alphabet) for strg in xrange(rounds))
+        self.play(myRandStr)
+
+    def playInteractive(self, winningScore = 10):
+
+        if winningScore == None:
+            winningScore = self.winningScore
 
         scoreComp = 0
         scorePlayer = 0
-        while scoreComp < self.goal and scorePlayer < self.goal:
+        winner = None
+        while scoreComp < winningScore and scorePlayer < winningScore:
             print "\n\n\n\n"
             print "------------------------------------------------------"
+
+            if  COMPUTER == winner:
+                print "I guessed correct! A point for me!"
+                scoreComp += 1
+            elif PLAYER == winner: #not blank else to prevent saying on first round
+                print "err... You fooled me this time! A point for you!"
+                scorePlayer += 1
+
             print "My score is: " + str(scoreComp) + ",   Your score is: " + str(scorePlayer)
-            print "type one of  "+str(self.domain)+",I will predict what you will type."
+            print "type one of  "+str(self.alphabet)+",I will predict what you will type."
             print "The following is the history, make the next character random!"
             print self.history
 
             playerChoice = raw_input()
 
-            if playerChoice not in self.domain:
-                print "No fair! you can only type stuff in "+str(self.domain)+"try again!"
+            if playerChoice not in self.alphabet:
+                print "\n\n\n\n"
+                print "------------------------------------------------------"
+                print "No fair! you can only type stuff in "+str(self.alphabet)+"try again!"
+                print "------------------------------------------------------"
                 continue
 
             compChoice = self.decideAction(playerChoice)
 
             winner = self.whoWinsRound(compChoice,playerChoice)
-            if  COMPUTER == winner:
-                print "I guessed correct! A point for me!"
-                scoreComp += 1
-            else:
-                print "err... You fooled me this time! A point for you!"
-                scorePlayer += 1
 
         self.gameOver(scorePlayer,scoreComp)
+        self.findProbs(self.history)
+        self.reset()
+
+    def findProbs(self,history):
+
+        print "here is my brain: "
+        print self.freqs
+
+        print "here is some analysis of your 'randomness'"
+        n = len(history)
+        freqs = defaultdict(lambda : 0)
+        for elem in history:
+            freqs[elem] += 1
+
+        for char,numOccurs in freqs.iteritems():
+            print char + " occurred with probability: " + str(numOccurs/n)
+
+
+    def setAlphabet(self):
+        alphabet = raw_input()
+        self.search=re.compile(r'[^'+alphabet+']').search
+        self.alphabet = list( alphabet )
+        self.freqs = defaultdict(lambda :[0]*len(alphabet))
+
+        for i,elem in enumerate(self.alphabet):
+            self.index[elem] = i
+
+        print "the alphabet is now " + alphabet
+
+    def setLookbehind(self):
+       print "What int amount of memory should I have"
+       self.lookbehind = int( raw_input() )
+
+
+
+    def reset(self):
+        self.freqs = defaultdict(lambda :[0]*len(self.alphabet))
+        self.history = ""
 
     def gameOver(self,scorePlayer,scoreComp):
         #game ended
@@ -152,4 +201,3 @@ class Predict:
         else:
             print "HAHAHAHAHAH I won! Silly deterministic machine!"
 
-        self.freqs = defaultdict(lambda :[0]*len(self.domain))
